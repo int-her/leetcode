48283c48a829a9c19f271c8a39dfcaec80631f92
int-her <valenti1424@snu.ac.kr>
2020-08-27 02:56:00 +0900

    Solve #384

---
+++ src/medium/Solution384.kt
@@ -0,0 +1,28 @@
+package medium
+
+import java.util.*
+
+class Solution384(val nums: IntArray) {
+    private val rand = Random()
+
+    /** Resets the array to its original configuration and return it. */
+    fun reset(): IntArray = nums
+
+    /** Returns a random shuffling of the array. */
+    fun shuffle(): IntArray {
+        return nums.clone().also { shuffled ->
+            shuffled.indices.forEach { index ->
+                val randIndex = rand.nextInt(shuffled.size - index) + index
+                shuffled[index] = shuffled[randIndex].also { shuffled[randIndex] = shuffled[index] }
+            }
+        }
+    }
+
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
